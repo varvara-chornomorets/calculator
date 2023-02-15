53d1fde23a8d319eb455e6c39a77fc6f2f6c54b1
YouPiterOn <oyushchenko@kse.org.ua>
2023-02-15 21:08:01 +0200

    something

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,29 @@
-﻿
-git status`     zdSGU,b
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = "";
+            var tokens = new string[100];
+            line = Console.ReadLine();
+            int k = 0;
+            for(int i = 0; i < line.Length; i++)
+            {
+                char s = line[i];
+                if (s == ' ')
+                {
+                    
+                    k++;
+                }
+                else
+                {
+                    tokens[k] += s;
+                }
+            }
+            Console.WriteLine(tokens);
+        }
+    }
+}
