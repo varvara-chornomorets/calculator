7c6f1921a1269019434aaa3caa5ef76dd93df2ab
varvara <vchornomorets@kse.org.ua>
2023-02-19 19:06:54 +0100

    added queue class

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -23,14 +23,3 @@ foreach (string token in tokens)
     Console.WriteLine(token);
 }
 
-
-
-var newList = new ArrayList();
-newList.Add(5);
-Console.WriteLine(newList.GetLenght());
-newList.Add(7);
-Console.WriteLine(newList.GetLenght());
-newList.Remove(7);
-Console.WriteLine(newList.GetLenght());
-newList.Add(689);
-Console.WriteLine(newList.GetElement(1));
---
+++ Calculator/Queue.cs
@@ -0,0 +1,43 @@
+﻿namespace Calculator;
+
+public class Queue
+{
+    private int _pointer = 0;
+    private static int _capacity = 50;
+    private int[] _array = new int[_capacity];
+
+    public void Enque(int value)
+    {
+        if (_pointer == _capacity)
+        {
+            throw new Exception("queue is overloaded");
+        }
+
+        _array[_pointer] = value;
+        _pointer++;
+
+    }
+
+    public void Deque()
+    {
+        if (_pointer == 0)
+        {
+            throw new Exception("queue is empty, but you try to take an element out");
+        }
+        int result = _array[0];
+        _pointer--;
+        for (int i = 0; i < _pointer && _pointer < 49; i++ )
+        {
+            _array[i] = _array[i + 1];
+        }
+    }
+
+    public void Print()
+    {
+        foreach (var number in _array)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+}
